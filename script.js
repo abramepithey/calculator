b3dd7d01a05a9d9487502067fd12638c3d887f85
abramepithey <abramepithey@gmail.com>
2025-06-13 14:31:00 -0600

    Implement basic arithmetic functions: add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNum = 0;
+let secondNum;
+let operand;
+
+const add = function(num1, num2) {
+    return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+
+const divide = function(num1, num2) {
+    return Number((num1 / num2).toFixed(5));
+};
